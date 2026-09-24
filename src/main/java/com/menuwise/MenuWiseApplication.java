3d06452e6deedc9e8f72faeef0a0c5a543ea457a
maruf50 <marufhossainkhan707@gmail.com>
2026-09-24 13:06:22 +0600

    Initial baseline setup: configure gitignore and project structure

--- src/main/java/com/menuwise/MenuWiseApplication.java
+++ src/main/java/com/menuwise/MenuWiseApplication.java
@@ -2,12 +2,8 @@ package com.menuwise;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
-@EnableCaching
-@EnableScheduling
 public class MenuWiseApplication {
 
     public static void main(String[] args) {
--- src/test/java/com/menuwise/MenuWiseApplicationTests.java
+++ src/test/java/com/menuwise/MenuWiseApplicationTests.java
@@ -1,12 +1,47 @@
 package com.menuwise;
 
+import com.menuwise.repository.CategoryRepository;
+import com.menuwise.repository.IngredientRepository;
+import com.menuwise.repository.ItemIngredientRepository;
+import com.menuwise.repository.ItemRepository;
+import com.menuwise.repository.SupplierRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class MenuWiseApplicationTests {
 
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Autowired
+    private IngredientRepository ingredientRepository;
+
+    @Autowired
+    private ItemRepository itemRepository;
+
+    @Autowired
+    private ItemIngredientRepository itemIngredientRepository;
+
+    @Autowired
+    private SupplierRepository supplierRepository;
+
     @Test
     void contextLoads() {
+        assertThat(categoryRepository).isNotNull();
+        assertThat(ingredientRepository).isNotNull();
+        assertThat(itemRepository).isNotNull();
+    }
+
+    @Test
+    void databaseSeeder_ShouldPopulateInitialData() {
+        assertThat(categoryRepository.count()).isGreaterThanOrEqualTo(4);
+        assertThat(supplierRepository.count()).isGreaterThanOrEqualTo(3);
+        assertThat(ingredientRepository.count()).isGreaterThanOrEqualTo(15);
+        assertThat(itemRepository.count()).isGreaterThanOrEqualTo(8);
+        assertThat(itemIngredientRepository.count()).isGreaterThanOrEqualTo(8);
     }
 }
